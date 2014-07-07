6ffd9b1043c7315b7cf85783ebd7076b4eea36da
Thomas Levine <_@thomaslevine.com>
2014-07-07 10:06:38 -0400

    -

--- join.py
+++ join.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import itertools
+import os
 from io import BytesIO
 from PIL import Image
 
@@ -18,29 +20,37 @@ def new_image_size(wav_length, ppm_dimensions, scale):
 
 def join(ppm_fn, wav_fn, out_fn):
     SCALE = 10
-    original_image = Image.open(ppm)
+    original_image = Image.open(ppm_fn)
     intermediate_ppm = BytesIO()
     wav = open(wav_fn, 'rb')
+    out = open(out_fn, 'wb')
 
-    original_image.resize(new_image_size(os.path.getsize(wav), i.size, SCALE))
-    original_image.save(intermediate_ppm)
+    size = new_image_size(os.path.getsize(wav_fn), original_image.size, SCALE)
+    columns, rows = size
+    original_image.resize(size)
+    original_image.save(intermediate_ppm, format = 'ppm')
 
     intermediate_ppm.seek(0)
-    while True:
-        image_pixel = intermediate_ppm.read(1)
-        if image_pixel == '':
-            break
-        for _ in range(SCALE):
-            audio_pixel = wav.read(1)
-            bytes([ord(image_pixel) + ord(audio_pixel)])
-
-    magicnumber = ppm.readline()
-    dimensions = ppm.readline()
-    maxcolor = ppm.readline()
+    magicnumber = intermediate_ppm.readline()
+    dimensions = intermediate_ppm.readline()
+    maxcolor = intermediate_ppm.readline()
     out.write(magicnumber)
     out.write(dimensions)
     out.write(maxcolor)
-    columns, rows = map(int,dimensions.decode('ascii').split(' '))
-    print(columns, rows)
+    intermediate_ppm.seek(0)
 
-# join(open('augustin.ppm', 'rb'), open('fms.wav', 'rb'), open('out.ppm', 'wb'))
+    while True:
+        half = lambda x: round(x / 2)
+        image_row = list(map(half, intermediate_ppm.read(columns)))
+        if len(image_row) < columns:
+            break
+
+        audio_row = itertools.chain(*[[half(ord(wav.read(1)))] * SCALE for _ in range(round(columns/SCALE))])
+        print(image_row, audio_row)
+
+        # Adjust them, and write it a few times
+        # out.write(bytes([a + b for a, b in zip(image_row, audio_row)] * SCALE))
+        # out.write(bytes([a + b for a, b in zip(image_row, audio_row)]))
+        out.write(bytes(image_row))
+
+join('augustin.ppm', 'fms.wav', 'out.ppm')
