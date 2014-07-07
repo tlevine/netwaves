79c620fb199d763adff0764a2841530049312b63
Thomas Levine <_@thomaslevine.com>
2014-07-07 10:49:32 -0400

    -

--- join.py
+++ join.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from math import ceil
 import itertools
 import os
 from io import BytesIO
@@ -22,14 +23,28 @@ def join(ppm_fn, wav_fn, out_fn):
     SCALE = 10
     ppm_fp = open(ppm_fn, 'rb')
     wav_fp = BytesIO()
-    for _ in range(3):
-        wav_fp.write(ppm_fp.readline())
+    wav_fp.write(ppm_fp.readline()) # magic number
+    columns, rows = map(int, ppm_fp.readline().decode('ascii').split(' '))
+    wav_size = os.path.getsize(wav_fn)
+    adj = (wav_size / (columns * rows)) ** 0.5
+    new_columns = ceil(columns / adj)
+    new_rows = ceil(rows / adj)
+    dimensions = ('%d %d\n' % (new_columns, new_rows)).encode('ascii')
+    wav_fp.write(dimensions)
+    wav_fp.write(ppm_fp.readline()) # max color
     ppm_fp.seek(0)
     for line in open(wav_fn, 'rb'):
         wav_fp.write(line)
+    remainder = (new_columns * new_rows - len(wav_fp.getvalue()))
+    print(remainder)
+    print(len(wav_fp.getvalue()))
+    wav_fp.write(bytes([127] * remainder))
     wav_fp.seek(0)
+
+    open('/tmp/a.ppm','wb').write(wav_fp.getvalue())
 
     ppm = Image.open(ppm_fp)
     wav = Image.open(wav_fp)
+    Image.blend(ppm, wav, 0.8).save(out_fn)
 
 join('augustin.ppm', 'fms.wav', 'out.ppm')
