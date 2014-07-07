1f6e25706a32f2aeadcdfcb3db09ca5c84b8ea8b
Thomas Levine <_@thomaslevine.com>
2014-07-07 03:27:50 -0400

    remainder

--- wav2ppm.hs
+++ wav2ppm.hs
@@ -6,8 +6,11 @@ import qualified Data.ByteString.Lazy         as B
 header :: Int -> Int -> B.ByteString
 header columns rows = pack $ unlines ["P6","32 40","255"]
 
+remainder :: Int -> Int -> B.ByteString
+remainder columns fileLength = pack $ replicate (fileLength `mod` columns) '\NUL'
+
 main :: IO ()
 main = do
-  contents <- B.readFile "fms_symphony.wav"
+  contents <- B.readFile "stuff.wav"
   B.putStr $ header 3 2
   B.putStr contents
