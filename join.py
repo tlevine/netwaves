4190242e214e4ccf8be8c99f6b212bed231ec2fc
Thomas Levine <_@thomaslevine.com>
2014-07-07 09:40:23 -0400

    yay

--- join.py
+++ join.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from io import BytesIO
 from PIL import Image
 
 def parser():
@@ -12,13 +13,26 @@ def main():
 
 def new_image_size(wav_length, ppm_dimensions, scale):
     columns, rows = ppm_dimensions
-    scale = (wav_length / (columns * rows) / scale) ** 0.5
-    return round(columns * scale), round(rows * scale)
+    adj = wav_length / (columns * rows)
+    return round(columns * adj ** 0.5 / scale), round(rows * adj ** 0.5 / scale)
 
-def join(ppm, wav, out):
-    i = Image.open(ppm)
-    i.size
-    new_image_size(os.path.getsize(wav), i.size)
+def join(ppm_fn, wav_fn, out_fn):
+    SCALE = 10
+    original_image = Image.open(ppm)
+    intermediate_ppm = BytesIO()
+    wav = open(wav_fn, 'rb')
+
+    original_image.resize(new_image_size(os.path.getsize(wav), i.size, SCALE))
+    original_image.save(intermediate_ppm)
+
+    intermediate_ppm.seek(0)
+    while True:
+        image_pixel = intermediate_ppm.read(1)
+        if image_pixel == '':
+            break
+        for _ in range(SCALE):
+            audio_pixel = wav.read(1)
+            bytes([ord(image_pixel) + ord(audio_pixel)])
 
     magicnumber = ppm.readline()
     dimensions = ppm.readline()
