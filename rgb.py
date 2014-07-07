ecd70a0bc63c50b2be2fa5df8de792e59c848a35
Thomas Levine <_@thomaslevine.com>
2014-07-07 04:42:08 -0400

    yay

--- rgb.py
+++ rgb.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 import itertools
 
-def tone(rgb, columns, rows, basename):
-    header_str = '''P6
-%d %d
-255
-''' % (columns, rows)
-    header = header_str.encode('ascii')
-    body = bytes(rgb * rows * columns)
-    with open('%s.wav' % basename, 'wb') as fp:
-        fp.write(body)
-    with open('%s.ppm' % basename, 'wb') as fp:
-        fp.write(header)
-        fp.write(body)
+def sink(columns, rows, wav_pointer, ppm_pointer):
+    try:
+        pointers = [ppm_pointer, wav_pointer]
+        header_str = 'P6\n%d %d255\n' % (columns, rows)
+        header = header_str.encode('ascii')
+        ppm_pointer.write(header)
+        count = 0
+        while True:
+            for pointer in pointers:
+                raw = (yield)
+                if isinstance(raw, int):
+                    result = [raw]
+                else:
+                    result = list(raw)
+                pointer.write(bytes(result))
+                pointer.flush()
+            count += len(result)
+    except GeneratorExit:
+        l = [127] * (columns * rows - count)
+        for pointer in pointers:
+            print(l)
+            pointer.write(bytes(l))
+            pointer.flush()
 
-tone([0,127,255], 800, 600, 'z')
+s = sink(800, 600, open('sink.wav', 'wb+'), open('sink.ppm', 'wb+'))
+next(s)
+s.send(0)
+s.send([127,255])
